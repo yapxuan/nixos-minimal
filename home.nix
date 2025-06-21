4e0e5c5209da3c7935c63edd6ea6096d193ff86d
yapxuan <puiyongqing@gmail.com>
2025-06-22 06:11:10 +0800

    modified alacritty config

--- home.nix
+++ home.nix
@@ -98,9 +98,13 @@
     # custom settings
     settings = {
       env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        colors.draw_bold_text_with_bright_colors = true;
+      font.size = 12;
+      colors = {
+        primary = {
+        background = "#181818";   
+        foreground = "#d8d8d8";   
+        };
+        draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
