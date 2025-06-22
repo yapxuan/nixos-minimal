9c5f2cb588af3c8adfa2cb213aeeb0dd64cb0fa1
yapxuan <puiyongqing@gmail.com>
2025-06-22 12:05:46 +0800

    update bashrc in home manager

--- home.nix
+++ home.nix
@@ -157,26 +157,14 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    #   # TODO add your custom bashrc here
-    #   bashrcExtra = ''
-    #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    #   '';
+    bashrcExtra = ''
+      fastfetch
+    '';
 
-    #   # set some aliases, feel free to add more or remove some
-    #   shellAliases = {
-    #     k = "kubectl";
-    #     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-    #     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-    #   };
+    shellAliases = {
+      nix = "nom";
+    };
   };
 
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.05";
 }
