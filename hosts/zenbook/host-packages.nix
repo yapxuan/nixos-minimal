ee7f4e2b9f8c1c0c0855788f4881d2c528d7d987
yapxuan <puiyongqing@gmail.com>
2025-06-28 07:59:42 +0800

    refactor structure

---
+++ hosts/zenbook/host-packages.nix
@@ -0,0 +1,15 @@
+{ pkgs, inputs, ... }:
+
+{
+  nixpkgs.overlays = [ inputs.fenix.overlays.default ];
+  environment.systemPackages = with pkgs; [
+    (fenix.complete.withComponents [
+      "cargo"
+      "clippy"
+      "rustc"
+      "rustfmt"
+    ])
+    rust-analyzer-nightly
+    inputs.helix.packages."${pkgs.system}".helix
+  ];
+}
---
+++ modules/home/default.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+  imports = [
+    ./alacritty.nix
+    ./bash.nix
+    ./dconf.nix
+    ./helix.nix
+    ./homepkgs.nix
+    ./nh.nix
+    ./starship.nix
+    ./zoxide.nix
+    ./zsh.nix
+    ./git.nix
+    ./firefox.nix
+  ];
+}
