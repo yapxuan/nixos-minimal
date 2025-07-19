81bdc39453bc17138527aeb6789f8d25ee1ed6f8
yapxuan <puiyongqing@gmail.com>
2025-07-20 06:09:46 +0800

    fix typo

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -13,8 +13,7 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "yapxuan.cachix.org-1:patwx9r3ULo9bqp+70S01APEvpw2z20la9t/okouaRg="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "watersucks.cachix.org-1:6gadPC5R8iLWQ3EUtfu3GFrVY7X6I4Fwz/ihW25Jbv8=
-"
+        "watersucks.cachix.org-1:6gadPC5R8iLWQ3EUtfu3GFrVY7X6I4Fwz/ihW25Jbv8="
       ];
       trusted-users = [
         "root"
